840644a6434e1d00e5b2544d2d4f01076de793eb
Ramome <ra-mom2000@hotmail.com>
2024-02-05 22:16:23 -0300

    first commit

---
+++ src/components/note-card.tsx
@@ -0,0 +1,23 @@
+export function NoteCard() {
+  return (
+    <button className="text-left rounded-md bg-slate-800 p-5 space-y-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">Há 4 dias</span>
+      <p className="text-sm leading-6 text-slate-400">
+        Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi
+        eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu
+        reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma
+        uma pindureta. Mussum Ipsum, cacilds vidis litro abertis. Per aumento de
+        cachacis, eu reclamis. Todo mundo vê os porris que eu tomo, mas ninguém
+        vê os tombis que eu levo! Suco de cevadiss deixa as pessoas mais
+        interessantis. Admodum accumsan disputationi eu sit. Vide electram
+        sadipscing et per. Mé faiz elementum girarzis, nisi eros vermeio. Si u
+        mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nec orci
+        ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum
+        felis. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus
+        tristique interdum.
+      </p>
+
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+    </button>
+  )
+}
