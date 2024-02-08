b24510224c8afdee9a44c8dd87c11c061861e506
Ramome <ra-mom2000@hotmail.com>
2024-02-07 22:27:27 -0300

    improvements

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -10,6 +10,7 @@ interface NewNoteCardProps {
 let speechRecognition: SpeechRecognition | null = null
 
 export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
+  const [open, setOpen] = useState(false)
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
   const [isRecording, setIsRecording] = useState(false)
   const [content, setContent] = useState('')
@@ -37,6 +38,8 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
 
     setContent('')
     setShouldShowOnboarding(true)
+
+    setOpen(false)
 
     toast.success('Nota criada com sucesso! 🎉')
   }
@@ -89,7 +92,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   }
 
   return (
-    <Dialog.Root>
+    <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger className="relative overflow-hidden rounded-md bg-slate-700 p-5 gap-3 text-left flex flex-col hover:ring-2 outline-none hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 group">
         <span className="text-sm font-medium text-slate-200">
           Adicionar nota
@@ -158,7 +161,8 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                 <button
                   type="button"
                   onClick={handleSaveNote}
-                  className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+                  className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500 disabled:text-slate-300 disabled:bg-slate-800 disabled:cursor-not-allowed"
+                  disabled={content === ''}
                 >
                   Salvar nota
                 </button>
